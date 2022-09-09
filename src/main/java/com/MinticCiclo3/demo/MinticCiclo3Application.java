151eb309372719b1bb19a5122c2abbb148e0b7fa
Esteban2618 <estebanc2618@gmail.com>
2022-09-08 23:49:09 -0500

    Agregar la estructura de springBoot

---
+++ src/main/java/com/MinticCiclo3/demo/MinticCiclo3Application.java
@@ -0,0 +1,14 @@
+package com.MinticCiclo3.demo;
+
+/* import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+ */
+
+public class MinticCiclo3Application {
+
+	//public static void main(String[] args) {
+		//SpringApplication.run(MinticCiclo3Application.class, args);
+	}
+
---
+++ src/test/java/com/MinticCiclo3/demo/MinticCiclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MinticCiclo3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinticCiclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
